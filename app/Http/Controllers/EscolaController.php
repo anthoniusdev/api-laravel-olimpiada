c83aaefc50652d5ff838f8782b7c3ea65f78440f
Anthonius <anthoniusmiguel@gmail.com>
2024-03-18 19:18:41 -0300

    iniciando os trabalhos

--- app/Http/Controllers/EscolaController.php
+++ app/Http/Controllers/EscolaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Escola;
 use Illuminate\Support\Str;
+use App\Models\geraCodigoEscola;
 
 class EscolaController extends Controller
 {
