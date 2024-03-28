e8a06de34346ccb386ba852d01ae1dc878d41cb3
Anthonius <anthoniusmiguel@gmail.com>
2024-03-28 01:11:02 -0300

    resolvendo erro de CORS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,34 +15,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $allowedOrigins = [
-            'https://olimpiadasdosertaoprodutivo.com/',
-        ];
-
-        $allowedMethods = [
-            'GET',
-            'POST',
-            'PUT',
-            'DELETE',
-            'OPTIONS',
-        ];
-
-        $allowedHeaders = [
-            'Content-Type',
-            'Authorization',
-            'X-Requested-With',
-        ];
-
-        $origin = $request->headers->get('Origin');
-
-        if (in_array($origin, $allowedOrigins)) {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', 'https://olimpiadasdosertaoprodutivo.com')
-                ->header('Access-Control-Allow-Methods', '*')
-                ->header('Access-Control-Allow-Headers', '*')
-                ->header('Access-Control-Allow-Credentials', 'true');
-        }
-
-        return abort(403, 'Origem não permitida.');
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin');
     }
 }
