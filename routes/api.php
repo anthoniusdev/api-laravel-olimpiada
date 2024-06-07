31b66a7fe3fa8d7dc2d8b2ea9e2fb30449ab95fe
Anthonius <anthoniusmiguel@gmail.com>
2024-06-06 23:34:50 -0300

    merge branch savio with conflits resolved

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin;
 use App\Http\Controllers\AlunoController;
 use App\Http\Controllers\EscolaController;
 use App\Http\Controllers\ProvaController;
@@ -7,12 +8,13 @@ use App\Http\Controllers\QuestaoTemporariaController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::post('admin/login', [Admin::class, "login"]);
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::get('/verify-login', function () {
-//         return ["isAuthenticated" => true];
-//     });
-// });
+Route::post('admin/login', [Admin::class, "login"]);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/verify-login', function () {
+        return ["isAuthenticated" => true];
+    });
+});
+
 
 Route::prefix('/escola')->group(function () {
     Route::post('/cadastro', [EscolaController::class, 'store']);
@@ -35,7 +37,8 @@ Route::prefix('/aluno')->group(function () {
             Route::post('/iniciar_prova', [ProvaController::class, 'iniciarProva']);
             Route::post('finalizar_prova', [ProvaController::class, 'finalizarProva']);
             Route::post('/add_prova', [ProvaController::class, 'store']);
-            Route::get('/verifica_tempo', [AlunoController::class, 'verificarTempoProva']);
+                        Route::get('/verifica_tempo', [AlunoController::class, 'verificarTempoProva']);
+
             Route::get('/prova_respondida', [AlunoController::class, 'validarProvaRespondida']);
             Route::prefix('/questao')->group(function(){
                 Route::get('/', [AlunoController::class, 'obterQuestaoAleatoria']);
@@ -44,7 +47,6 @@ Route::prefix('/aluno')->group(function () {
         });
     });
 });
-
 // Rota para obter as imagens
 Route::prefix('/img')->group(function () {
     // Rota para obter as imagens publicas
